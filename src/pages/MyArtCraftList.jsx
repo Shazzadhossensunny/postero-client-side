6a5ed7adbc53ebc59d0a29afa1dd4df6746341da
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2024-04-27 03:45:00 +0600

    my art and cart data show

--- src/pages/MyArtCraftList.jsx
+++ src/pages/MyArtCraftList.jsx
@@ -1,6 +1,21 @@
+import { useContext, useEffect, useState } from "react"
+import { AuthContext } from "../Context/AuthContextComponent"
 
 
 export default function MyArtCraftList() {
+
+  const {user} = useContext(AuthContext) || {};
+  const [userItems, setUserItems] = useState([])
+  useEffect(()=>{
+    fetch(`http://localhost:5000/myProduct/${user?.email}`)
+    .then(res => res.json())
+    .then(data => {
+      setUserItems(data)
+    })
+  },[user])
+
+
+
   return (
     <div>MyArtCraftList</div>
   )
