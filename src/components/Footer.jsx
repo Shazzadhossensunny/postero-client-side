3defaa0d2adf46f5819e52a09d64cd9ef892b477
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2024-04-28 01:28:57 +0600

    footer add contact info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,8 +8,19 @@ export default function Footer() {
       <aside className="space-y-6">
         <img src={logo} alt="" />
         <p>
-          Copyright © 2024 <span>Postero</span>. - All right reserved
+          Copyright © 2024 <span className="text-orange-400">Sunny</span>. - All right reserved
         </p>
+        <div>
+          <h2 className="text-lg font-semibold mb-3">General Inquiries</h2>
+          <p>
+
+          <a href="mailto:contact@info.com" className="btn-link">contact@info.com</a>
+          </p>
+          <p>
+
+          <a href="tel:+985648274557" className="btn-link">+985648274557</a>
+          </p>
+        </div>
         <div className="flex text-2xl gap-5 cursor-pointer">
           <FaFacebookF id="my-anchor-facebook"></FaFacebookF>
           <Tooltip
