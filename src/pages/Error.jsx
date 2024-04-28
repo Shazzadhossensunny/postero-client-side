fc7fc0a4e9ad7ed542e44221627702829e863e5c
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2024-04-28 18:28:47 +0600

    firebase issue fixed

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -6,7 +6,7 @@ export default function Error() {
   return (
     <div className="container mx-auto my-9">
       <div>
-        <img className='h-[700px] mx-auto' src={errorImg} alt="" />
+        <img className='h-full md:h-[400] lg:h-[700px] mx-auto' src={errorImg} alt="" />
       </div>
       <Link to='/' className='flex justify-center'>
        <button className='btn btn-warning'>Go to Home</button>
